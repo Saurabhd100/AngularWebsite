38cc474fde37b65c992c8ba59665fd07d8302dd5
Sandeshwar <ssandeshwar@gmail.com>
2018-12-05 20:36:03 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { BannerComponent } from './banner/banner.component';
+import { HeaderComponent } from './header/header.component';
+import { SpecialComponent } from './special/special.component';
+import { AboutComponent } from './about/about.component';
+import { MenuComponent } from './menu/menu.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { TeamComponent } from './team/team.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationBarComponent,
+    BannerComponent,
+    HeaderComponent,
+    SpecialComponent,
+    AboutComponent,
+    MenuComponent,
+    GalleryComponent,
+    TeamComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
